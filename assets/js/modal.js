1b67c9c638bdbac9c3a99efa6ebe0f53f9fc3c4e
Xersaix <karl_kondamoyen@yahoo.fr>
2023-05-12 20:00:08 +0200

    Ajout menu 60%

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,12 +1,25 @@
 
 // Get the modal
 var modal = document.getElementById("myModal");
+var pokeball = document.getElementById("menu-pokeball");
+var pokeball_text = document.querySelector("#pokeball-text");
+var fight = document.getElementById("fight-button")
 
 // Get the button that opens the modal
 
 
-
 // When the user clicks the button, open the modal 
-window.onload = function() {
-  modal.style.display = "block";
+window.onload = function () {
+    modal.style.display = "block";
 }
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == fight) {
+        modal.style.display = "none";
+    }
+}
+
+fight.addEventListener("click",function(){
+    modal.style.display = "none";
+})
